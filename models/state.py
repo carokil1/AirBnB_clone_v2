fee10a09a99eebcccb5303ea99f9f16e4fa6185e
Anthony <bondolo90@gmail.com>
2023-09-20 05:47:25 -0700

    Commit 13: Task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State Module for HBNB project"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
